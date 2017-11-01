fe02eca29d80d9b0c33d7e1d0a477ff07c945420
Oleh <pichyn7@gmail.com>
2017-11-01 11:37:52 +0200

    main

---
+++ src/main/main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by Oleh on 01.11.2017.
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+    }
+}
