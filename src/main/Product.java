b98227f92892287e3a4a4849239e87eaefa4068c
Oleh <pichyn7@gmail.com>
2017-11-02 11:55:00 +0200

    1111

---
+++ src/main/Product.java
@@ -0,0 +1,17 @@
+package main;
+
+/**
+ * Created by Oleh on 02.11.2017.
+ */
+public class Product {
+
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
