4b8d19dd549abfe4f0da552e44385ccb367daced
Oleh <pichyn7@gmail.com>
2017-11-02 10:04:41 +0200

    add line ksm-31

--- src/main/main.java
+++ src/main/main.java
@@ -7,6 +7,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hi ==  = = = = = = =  == =  ==  = == ");
+        System.out.println("ksm-31");
 
     }
 }
