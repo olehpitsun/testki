4fd3c39d661a3ca32f6f78e5be9592e91d1e299a
Oleh Pitsun <oleh.pitsun@plumrocket.com>
2022-12-01 15:39:04 +0200

    remove unneaded

--- src/SOLID/DI/ICarTransformer.java
+++
@@ -1,9 +0,0 @@
-package SOLID.DI;
-
-/**
- * Created by Oleh on 13.11.2017.
- */
-public interface ICarTransformer {
-
-    void toCar();
-}
--- src/patterns/composite/IAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package patterns.composite;
-
-/**
- * Created by Oleh on 11.11.2017.
- */
-public interface IAlgorithm {
-
-    void applyAlgorithm();
-}
