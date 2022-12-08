d42e15e0a797569b870e87b052d5eadc45df6e0a
Oleh Pitsun <oleh.pitsun@plumrocket.com>
2022-12-08 15:27:30 +0200

    function example

--- clearsession.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION["authenticated"] = 'false';
-$_SESSION["login"] = '';
-
-header('Location: index.php');
-?>
--- index.php
+++ index.php
@@ -1,14 +1,44 @@
 <html>
-    <head></head>
-    <body>
-        <form id="login" method="post" action="auth.php">
-            <label for="login">Login</label>
-            <input id="login" name="login" type="text" required>
+<head></head>
+<body>
 
-            <label for="password">Access code:</label>
-            <input id="password" name="password" type="password" required>
+<?php
 
-            <input type="submit" value="OK">
-        </form>
-    </body>
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+
+    function addFunction($num1, $num2 = NULL) {
+        $sum = $num1 + $num2;
+
+        if($num1 === NULL){
+            echo 'hello';
+        }
+
+        return $sum;
+    }
+    $return_value = addFunction(1);
+
+    echo 'Result = ' . $return_value;
+
+} else {
+    ?>
+
+
+    <form id="login" method="post" action="/">
+        <label for="login">Login</label>
+        <input id="login" name="num1" type="text" required>
+
+        <label for="password">Access code:</label>
+        <input id="password" name="num2" type="text" required>
+
+        <input type="submit" value="OK">
+    </form>
+
+    <?php
+}
+?>
+</body>
 </html>
