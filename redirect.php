bc42da1a85e400982567bc9b692bc4413e502283
Oleh Pitsun <oleh.pitsun@example.com>
2025-11-16 12:41:23 +0200

    securit1

--- redirect.php
+++ redirect.php
@@ -4,9 +4,7 @@ require __DIR__ . "/vendor/autoload.php";
 
 $client = new Google\Client;
 
-$client->setClientId("581123649038-unl355sa3d6lg5hfknaq1kihp7nvhfpi.apps.googleusercontent.com");
-$client->setClientSecret("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");
-$client->setRedirectUri("http://localhost/redirect.php");
+
 
 if ( ! isset($_GET["code"])) {
 
